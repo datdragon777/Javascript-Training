0d85dbbbe59c3bef082a9fc1c9e5d8cf43a29503
Nguyễn Đức Đạt <ducdat612001@gmail.com>
2023-06-09 15:20:14 +0700

    Blackjack game
    Example using arrays, objects, booleans, if-else statements, comparison and logical operators, for-loops

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,41 +1,64 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackjack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let cards = [];
+let sum = 0;
+let hasBlackjack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+let player = {
+    name: "Datker",
+    chips: 752
+}
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = `${player.name}: $${player.chips}`
+
+function getRandomCard() {
+  const randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+}
 
 const startGame = () => {
-    renderGame()
-}
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+};
 
 const renderGame = () => {
-    cardsEl.textContent = `Cards: `
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
-    }
+  cardsEl.textContent = `Cards: `;
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
 
-    sumEl.textContent = `Sum: ${sum}`
-    if( sum <= 20 ) {
-        message = "Do you want to draw a new card?"
-    } else if ( sum == 21) {
-        message = "Melm Mlem"
-        hasBlackjack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
-}
+  sumEl.textContent = `Sum: ${sum}`;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum == 21) {
+    message = "Melm Mlem";
+    hasBlackjack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+};
 
 const newCard = () => {
-    let card = 7
-    sum += card 
-    cards.push(card)
-    renderGame()
-}
-
+  if (isAlive === true && hasBlackjack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+};
