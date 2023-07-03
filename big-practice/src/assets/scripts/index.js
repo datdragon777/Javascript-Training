f72a2072ca56862fb31c5cd0cf12fe10c9162bca
datdragon777 <ducdat612001@gmail.com>
2023-07-03 09:59:58 +0700

    Update position code

--- big-practice/src/assets/scripts/index.js
+++ big-practice/src/assets/scripts/index.js
@@ -1,21 +1,22 @@
 const createJobBtn = document.getElementById("create-job__btn");
 const listContent = document.getElementById("list__content");
 const formContent = document.getElementById("form__content");
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
 createJobBtn.addEventListener("click", () => {
   listContent.style.display = "none";
   formContent.style.display = "block";
 });
 
-
-const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
-options.forEach(option =>{
-    option.addEventListener("click", ()=>{
-        let selectedOption = option.querySelector(".option-text").innerText;
-        sBtn_text.innerText = selectedOption;
-        optionMenu.classList.remove("active");
-    });
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = option.querySelector(".option-text").innerText;
+    sBtn_text.innerText = selectedOption;
+    optionMenu.classList.remove("active");
+  });
 });
