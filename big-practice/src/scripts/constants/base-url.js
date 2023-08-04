76e9ff840dde826daae896496626e7a0ffd5c651
datdragon777 <ducdat612001@gmail.com>
2023-08-04 10:47:42 +0700

    Update base-url

--- big-practice/src/scripts/constants/base-url.js
+++ big-practice/src/scripts/constants/base-url.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://my-json-server.typicode.com/datdragon777/json-server/jobs";
 export const PATH = "jobs";
