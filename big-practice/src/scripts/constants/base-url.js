6b3fbfcd59bd2362bd42bd69f8dce00ee4d2388f
datdragon777 <ducdat612001@gmail.com>
2023-08-07 21:34:46 +0700

    Update base url

--- big-practice/src/scripts/constants/base-url.js
+++ big-practice/src/scripts/constants/base-url.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://dp7ppy-8080.csb.app";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://dp7ppy-8080https://json-server-vercel-blue-eight.vercel.app/.csb.app";
+// export const BASE_URL = "http://localhost:3000";
 export const PATH = "jobs";
